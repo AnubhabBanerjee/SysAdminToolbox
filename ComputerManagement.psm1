5095b3f1ed6c2b206572af717280bf069796e72e
Johannes Koenig <dev@jokoenig.de>
2018-09-04 20:45:35 +0200

    Manifest fixed

--- ComputerManagement.psm1
+++ ComputerManagement.psm1
@@ -1,6 +1,6 @@
 
 # Load Functions
-. .\functions\Disable-TunnelingProcotols.ps1
+. ($PSScriptRoot + "\functions\Disable-TunnelingProcotols.ps1")
 
 # Export Module Member Functions
 Export-ModuleMember -Function 'Disable-TunnelingProtocols'

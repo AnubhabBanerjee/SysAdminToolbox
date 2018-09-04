0df88aeec180eeb473ca5cb38d91fca411445907
Johannes Koenig <dev@jokoenig.de>
2018-09-04 19:59:55 +0200

    Disable-TunnelingProcotols.ps1 created

---
+++ functions/Disable-TunnelingProcotols.ps1
@@ -0,0 +1,20 @@
+function global:Disable-TunnelingProtocols {
+
+    <#
+    .SYNOPSIS
+    Disabled the tunneling protocols Teredo, Isatap and 6to4
+    #>
+
+    Process {
+        if ((Get-NetIsatapConfiguration).State -eq 'Disabled') {
+            Set-NetIsatapConfiguration -State Disabled
+        }
+        if ((Get-NetTeredoConfiguration).Type -ne 'Disabled') {
+            Set-NetTeredoConfiguration -Type Disabled
+        }
+        if ((Get-Net6to4Configuration).State -ne 'Disabled') {
+            Set-Net6to4Configuration -State Disabled
+        }
+        
+    }
+}
